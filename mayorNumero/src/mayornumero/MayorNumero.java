fbe3bdd06c485dc1b5907d5642c4ebc634099659
german <german001101@gmail.com>
2019-09-09 11:57:11 -0300

    CocienteResto

--- mayorNumero/src/mayornumero/MayorNumero.java
+++ mayorNumero/src/mayornumero/MayorNumero.java
@@ -1,9 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * find number 
  */
 package mayornumero;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +15,21 @@ public class MayorNumero {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        // TODO code application logic here 
+        int num, num2, result;
+        //Requesting data
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese el primer número: ");
+        num = input.nextInt();
+        System.out.println("Ingrese el segundo número: ");
+        num2 = input.nextInt();
+        //Operating
+        if(num > num2){
+            System.out.println("\nEl numero mayor es: " + num);
+        }else{
+            System.out.println("\nEl numero mayor es: " +  num2);
+        }
+            
+        }
     
 }
